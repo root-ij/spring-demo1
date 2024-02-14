8057189771973d1074dd9be5428a6792d7954c03
root-ij <irene.ijoo@gmail.com>
2024-02-14 14:49:03 +0900

    Initial commit IntelliJ로 Spring Maven Project 생성 자바 11, 버전 5.2.3으로 세팅

---
+++ src/main/java/com/ex/store/review/ReviewController.java
@@ -0,0 +1,10 @@
+package com.ex.store.review;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReviewController {
+
+
+}
